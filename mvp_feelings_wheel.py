3fe7267bb4dd10ddc64339274d6c994330df3c3a
Adrian Nguyen <tilounguyen@hotmail.com>
2023-11-30 11:10:18 -0500

    Wrote assertion for emotion csv and the function

--- mvp_feelings_wheel.py
+++ mvp_feelings_wheel.py
@@ -1,6 +1,5 @@
-from transformers import pipeline
-from icecream import ic
 import csv
+from transformers import pipeline
 
 
 def receive_console_input():
@@ -8,40 +7,50 @@ def receive_console_input():
     Receives the text which the user wishes to submit for processing
     :return:
     """
-
     input_text = input("Enter the text which needs to be decoded for emotions: ")
     return input_text
 
 
 def process_emotion(text):
-    # Sets up the emotion decoding model then parses it for emotions
+    """
+    Sets up the emotion decoding model then parses it for emotions
+    :return:
+    """
     classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
     model_outputs_results = classifier(text)
 
     return model_outputs_results
 
 
+def get_emotion_definitions():
+    dict_emotion = {}
+
+    with open(file='emotion_definition.csv', mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            key = row['emotion']
+            value = row['definition']
+
+            dict_emotion[key] = value
+
+    # Testing the returned values from the CSV
+    test_dict_emotions = {
+        'admiration': "Finding something impressive or worthy of respect.",
+        'amusement': "Finding something funny or being entertained."
+    }
+
+    assert dict_emotion.get("admiration") == test_dict_emotions.get("admiration"), "Imported CSV should match GoEmotions taxonomy definitiions"
+
+    return dict_emotion
+
 def output_results():
     pass
 
-
-def parse_emotion_dictionary():
-    """
-    Reads a csv which contains labeled emotions, the desired display label
-    and its associated definition and places it into a dictionary
-    :return dict_emotion_defs:
-    """
-
-    csv_filename = 'emotion_definition.csv'
-
-    with open(csv_filename) as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            print(row)
-
-
 def main():
-    dict_emotions = parse_emotion_dictionary()
+
+    dict_emotion = get_emotion_definitions()
+
+    print(dict_emotion)
 
 
 if __name__ == "__main__":
