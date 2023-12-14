ff26760aa2af40ebefe0ad12ae72b2afc59ca843
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-14 12:28:14 -0500

    Removed old code which was not part of class

--- minimal_feelings.py
+++ minimal_feelings.py
@@ -81,79 +81,6 @@ class DecodedEmotion:
                 result['label_definition'] = emotion_definition
 
 
-
-def process_emotions(text):
-    print("Running emotion classifier")
-    classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
-    model_outputs_results = classifier(text)
-    results = [{'input_text': text, 'scores': scores} for text, scores in zip(text, model_outputs_results)]
-
-    return results
-
-
-def filter_model_results(results, acceptable_score_threshold):
-    filtered_results = []
-
-    # Only keep the model results which pass a certain value
-    for item in results:
-        original_scores_list = item['scores']
-        temp_list = []
-        for label in original_scores_list:
-            if label['score'] > acceptable_score_threshold:
-                temp_list.append(label)
-
-        filtered_item = {'input_text': item['input_text'],
-                         'clean_scores': temp_list,
-                         'accepted_threshold': acceptable_score_threshold}
-        filtered_results.append(filtered_item)
-
-    return filtered_results
-
-
-def print_results_definitions(model_results):
-    emotions_dict = {
-        'admiration': "Finding something impressive or worthy of respect.",
-        'amusement': "Finding something funny or being entertained.",
-        'anger': "A strong feeling of displeasure or antagonism.",
-        'annoyance': "Mild anger, irritation.",
-        'approval': "Having or expressing a favorable opinion.",
-        'caring': "Displaying kindness and concern for others.",
-        'confusion': "Lack of understanding, uncertainty.",
-        'curiosity': "A strong desire to know or learn something.",
-        'desire': "A strong feeling of wanting something or wishing for something to happen.",
-        'disappointment': "Sadness or displeasure caused by the nonfulfillment of one’s hopes or expectations.",
-        'disapproval': "Having or expressing an unfavorable opinion.",
-        'disgust': "Revulsion or strong disapproval aroused by something unpleasant or offensive.",
-        'embarrassment': "Self-consciousness, shame, or awkwardness.",
-        'excitement': "Feeling of great enthusiasm and eagerness.",
-        'fear': "Being afraid or worried.",
-        'gratitude': "A feeling of thankfulness and appreciation.",
-        'grief': "Intense sorrow, especially caused by someone’s death.",
-        'joy': "A feeling of pleasure and happiness.",
-        'love': "A strong positive emotion of regard and affection.",
-        'nervousness': "Apprehension, worry, anxiety.",
-        'optimism': "Hopefulness and confidence about the future or the success of something.",
-        'pride': "Pleasure or satisfaction due to one's own achievements or the achievements of those with whom one is closely associated.",
-        'realization': "Becoming aware of something.",
-        'relief': "Reassurance and relaxation following release from anxiety or distress.",
-        'remorse': "Regret or guilty feeling.",
-        'sadness': "Emotional pain, sorrow.",
-        'surprise': "Feeling astonished, startled by something unexpected."
-    }
-
-    # Prints associated definition based on emotion labels found
-    for item in model_results:
-        scores = item['clean_scores']
-
-        for label_item in scores:
-            if label_item['label'] in emotions_dict:
-                emotion_name = label_item['label']
-                emotion_definition = emotions_dict.get(emotion_name)
-                print(f'{emotion_name}: {emotion_definition}')
-
-    return None
-
-
 def main():
     # input_text = input("Enter the text which needs to be decoded for emotions: ")
     input_text = [
@@ -174,12 +101,8 @@ def main():
 
     decoded_sentence = DecodedEmotion(test_sentence)
     decoded_sentence.print_raw_scores()
-    decoded_sentence.add_label_definitions()
+    decoded_sentence.add_label_definitions() # TODO: Incorporate into the class behavior itself?
     decoded_sentence.print_raw_scores()
-    # decoded_sentence.filter_scores()
-    # decoded_sentence.print_filtered_scores()
-
-
 
 
 
