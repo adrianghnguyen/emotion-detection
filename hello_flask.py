d0bb0d7823a75c3df32b3a6cf4ed87285713cfdf
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-14 16:44:57 -0500

    Playing around with Flask

--- hello_flask.py
+++ hello_flask.py
@@ -1,17 +1,21 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__) # What does this do?
 
 @app.route("/")
 def home():
-    return 'Howdy this is some text for a hello world</a>'
+    return render_template("index.html") # This allows me call an index file to be rendered instead of writing the HTML directly in the return string.
 
 @app.route("/input/<input_text>")
 def print_input(input_text):
-    return f"Here is the input text: {input_text}"
+    return render_template("python_in_page.html", content=input_text)
 
 @app.route("/admin")
 def admin():
+    """
+    An example where going to the /admin page redirects to the home function which loads "/"
+    :return:
+    """
     return redirect(url_for("home"))
 
 @app.route("/joke")
