e1e349480b4d34f5c5cb889587a9423c81a640a9
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-15 10:49:48 -0500

    Playing with text input form

--- feelings_wheel_web_app.py
+++ feelings_wheel_web_app.py
@@ -1,6 +1,15 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import *
 
 app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        print(request.form["input_text"])
+
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run()
