b15cb8fec70953b4b9557c1dc8fb9b48714db4c7
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-15 13:45:48 -0500

    Hard-coding test input value to be processed

--- feelings_wheel_web_app.py
+++ feelings_wheel_web_app.py
@@ -15,7 +15,20 @@ def home():
     if request.method == "POST":
         print(request.form["input_text"])
         input_text = request.form['input_text']
-        input_text = 'I am feeling sad today'
+        input_text = ("So I met a girl through bumble, she and I had an amazing vibe. I really liked her but in the "
+                      "long run, I was planning to go to Yale for my masters and she decided to stay in India. For "
+                      "three months I treated her with outmost respect, love and care. I honestly tried to be there "
+                      "for her always. Before dating me she was in a very toxic relationship and her ex was very "
+                      "abusive. She kinda started seeing a few guys after that ex, and she started seeing her "
+                      "classmate who apparently at that time only wanted a causal relationship. So she denied it to "
+                      "him. After a few days she met me. And I became her best friend for three months. I tried to be "
+                      "a realistic person with her, always gave her space and respected boundaries. So I went back "
+                      "home after three months of situationship and apparently she made out with the guy who just "
+                      "wanted casual. So next day she broke up with me telling me it was my fault. Later she told me "
+                      "that she fucked up and I forgave her and tried to fix things. She told me she loves me and she "
+                      "would never be with the other guy who is his classmate. Today I saw them together after "
+                      "college and she saw me. I texted her I was a standby for you, I hope you find your happiness "
+                      "and thanks for everything. She said I hope the same for you. So yeah, fuck her.") # TODO: Remove this hard-coded value after testing
         return redirect(f"/process/{input_text}")
 
     return render_template("index.html")
