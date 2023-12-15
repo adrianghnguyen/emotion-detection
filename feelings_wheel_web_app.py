d8d13754051c083f4e3f0813040fc6076a12f066
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-15 11:10:21 -0500

    Sending information into post request example

--- feelings_wheel_web_app.py
+++ feelings_wheel_web_app.py
@@ -2,13 +2,18 @@ from flask import *
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         print(request.form["input_text"])
+        text = request.form['input_text']
+        return redirect(f"/process/{text}")
 
     return render_template("index.html")
+
+@app.route("/process/<text>")
+def process(text):
+    return f"You sent \"{text}\" to be processed"
 
 
 if __name__ == "__main__":
