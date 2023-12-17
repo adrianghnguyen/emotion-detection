35a48ebf0e3d71ef422643dec42b64d159dbb935
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-16 20:56:18 -0500

    Added testing=True variable which uses placeholder values.

--- feelings_wheel_web_app.py
+++ feelings_wheel_web_app.py
@@ -2,12 +2,7 @@ from flask import *
 from minimal_feelings import DecodedEmotion
 
 app = Flask(__name__)
-
-def test_function(): # TODO: Get rid of this
-    print("I am executing a test function")
-    result = "test function result"
-
-    return result
+testing = True
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -15,26 +10,31 @@ def home():
     if request.method == "POST":
         print(request.form["input_text"])
         input_text = request.form['input_text']
-        input_text = ("So I met a girl through bumble, she and I had an amazing vibe. I really liked her but in the "
-                      "long run, I was planning to go to Yale for my masters and she decided to stay in India. For "
-                      "three months I treated her with outmost respect, love and care. I honestly tried to be there "
-                      "for her always. Before dating me she was in a very toxic relationship and her ex was very "
-                      "abusive. She kinda started seeing a few guys after that ex, and she started seeing her "
-                      "classmate who apparently at that time only wanted a causal relationship. So she denied it to "
-                      "him. After a few days she met me. And I became her best friend for three months. I tried to be "
-                      "a realistic person with her, always gave her space and respected boundaries. So I went back "
-                      "home after three months of situationship and apparently she made out with the guy who just "
-                      "wanted casual. So next day she broke up with me telling me it was my fault. Later she told me "
-                      "that she fucked up and I forgave her and tried to fix things. She told me she loves me and she "
-                      "would never be with the other guy who is his classmate. Today I saw them together after "
-                      "college and she saw me. I texted her I was a standby for you, I hope you find your happiness "
-                      "and thanks for everything. She said I hope the same for you. So yeah, fuck her.") # TODO: Remove this hard-coded value after testing
+
+        if testing:
+            input_text = ("So I met a girl through bumble, she and I had an amazing vibe. I really liked her but in the "
+                          "long run, I was planning to go to Yale for my masters and she decided to stay in India. For "
+                          "three months I treated her with outmost respect, love and care. I honestly tried to be there "
+                          "for her always. Before dating me she was in a very toxic relationship and her ex was very "
+                          "abusive. She kinda started seeing a few guys after that ex, and she started seeing her "
+                          "classmate who apparently at that time only wanted a causal relationship. So she denied it to "
+                          "him. After a few days she met me. And I became her best friend for three months. I tried to be "
+                          "a realistic person with her, always gave her space and respected boundaries. So I went back "
+                          "home after three months of situationship and apparently she made out with the guy who just "
+                          "wanted casual. So next day she broke up with me telling me it was my fault. Later she told me "
+                          "that she fucked up and I forgave her and tried to fix things. She told me she loves me and she "
+                          "would never be with the other guy who is his classmate. Today I saw them together after "
+                          "college and she saw me. I texted her I was a standby for you, I hope you find your happiness "
+                          "and thanks for everything. She said I hope the same for you. So yeah, fuck her.") # TODO: Remove this hard-coded value after testing
+
+        print(f'Received the following input text: {input_text}')
         return redirect(f"/process/{input_text}")
 
     return render_template("index.html")
 
 @app.route("/process/<text>")
 def process_emotion(text):
+
     user_emotion = DecodedEmotion(input_sentence=text) # More descriptive variable name needed?
     user_emotion.add_label_definitions() # FUCKING SPAGHETTITTITITITITI
     user_emotion.filter_scores() # Get rid of this into the class behavior. The class should have everything packed in.
@@ -42,10 +42,7 @@ def process_emotion(text):
     filtering_threshold = getattr(user_emotion, 'acceptable_score_threshold')
     print(emotion_results)
 
-    for emotion in emotion_results:
-        print(f'{emotion.get("label")} ({emotion.get("label_definition")}) presence detected with a score of {emotion.get("label_definition")}')
     return render_template("processed_text.html", emotion_results=emotion_results, input_text=text, filtering_threshold=filtering_threshold)
-    # return f"You sent \"{text}\" to be processed"
 
 
 if __name__ == "__main__":
