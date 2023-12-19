4080c2189ab7a4da1b695e5827a39c8ffede989b
Adrian Nguyen <tilounguyen@hotmail.com>
2023-12-18 20:05:26 -0500

    New version of form

--- flask_page.py
+++ flask_page.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
-    return render_template('form.html')
+    return render_template('index.html')
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
     return render_template('greeting.html', say=request.form['say'], to=request.form['to'])
+
+@app.route('/processed_emotion', methods=['GET', 'POST'])
+def processed_emotion():
+    return render_template('processed_text.html', input_text=request.form['input_text'])
 
 if __name__ == "__main__":
     app.run()
